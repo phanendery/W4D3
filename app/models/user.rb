08ad8a8adda661b9046795836abd216826001be7
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-04-03 17:32:38 -0700

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+  validates :user_name, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  after_initialize :ensure_session_token
+ 
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+
+    self.save!
+
+    self.session_token
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+      BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+
+  def self.find_by_credentials(user_name,password)
+    @user = User.find_by(user_name: user_name)
+    
+    if user
+      if is_password?(password)  
+        return user
+      else
+        #add error message
+      end
+    else
+      #add error message
+    end
+  end
+end
